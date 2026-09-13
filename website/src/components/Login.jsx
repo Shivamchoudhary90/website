fbb9484517b0218b008e72b18fc5394c6906509e
Shivamchoudhary90 <shivampatel9829@gmail.com>
2026-09-13 13:08:23 +0530

    Update Login.jsx

--- website/src/components/Login.jsx
+++ website/src/components/Login.jsx
@@ -45,7 +45,7 @@ function Login({ onSwitchToRegister }) {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://website-r1i5.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
