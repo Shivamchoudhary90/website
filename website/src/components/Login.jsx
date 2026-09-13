bfe5c71c9a07a04db8508dfa4dfbdbcee0ee4152
Shivamchoudhary90 <shivampatel9829@gmail.com>
2026-09-13 13:12:29 +0530

    Update Login.jsx

--- website/src/components/Login.jsx
+++ website/src/components/Login.jsx
@@ -45,7 +45,7 @@ function Login({ onSwitchToRegister }) {
     }
 
     try {
-      const res = await fetch("https://website-r1i5.onrender.com/register", {
+      const res = await fetch("https://website-r1i5.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
